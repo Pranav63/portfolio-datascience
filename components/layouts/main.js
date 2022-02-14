5972db063d5dadf10dfee284f55b4133efd6770a
Pranav Arora <pranav2vis@gmail.com>
2022-02-15 04:18:13 +0800

    change apos

--- components/layouts/main.js
+++ components/layouts/main.js
@@ -8,7 +8,7 @@ const Main= ({children, router}) => {
 		<Box as='main' pb={8}>
 			<Head>
 				<meta name='viewport' content='width=device-width, initial-scale=1'/>
-				<title>Pranav's Home</title>
+				<title>Pranav`&apos;` Home</title>
 
 			</Head>
 
